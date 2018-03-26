fb3c990eb7e5a0b4fa45789c01fb767abbf6c187
Shawn Sustrich <maxximillian@gmail.com>
2018-03-26 10:10:39 -0700

    Now with history! Press left mouse button for a new or the next spirograph, press teh right mouse botton for the previous spirograph.

--- src/Spirograph.java
+++ src/Spirograph.java
@@ -9,42 +9,69 @@ public class Spirograph extends PApplet {
 		PApplet.main(new String[] {"Spirograph"});
 	}
 	
-	float theta = (float) 1.5708;
-	float resolution = (float) .01;
-	float R = 0;
-	float r = 0;
-	float a = 0;
-	int red = 255;
-	int green = 255;
-	int blue = 255;
-	String s;
-	ArrayList<PVector> path = new ArrayList<PVector>();
-	Color c;
-
+	
+	private float theta = (float) 1.5708;
+	private float resolution = (float) .01;
+	private float R = 0;
+	private float r = 0;
+	private float a = 0;
+	private int red = 255;
+	private int green = 255;
+	private int blue = 255;
+	private String s;
+	private ArrayList<PVector> path = new ArrayList<PVector>();
+	private ArrayList<PVector> history =  new ArrayList<PVector>();
+	private Color c;
+	private int itteration = 1;
+	
+	
 	public void settings() {
 		  size(600, 600);
 		 path = new ArrayList<PVector>();
+	    	R =  (float) Math.floor(Math.random()*(200));
+	    	r =  (float) Math.floor(Math.random()*(200));
+	    	a =  (float) Math.floor(Math.random()*(200));
+	    	
+	    	red = (int) Math.floor(Math.random()*(255-100+1)+100);
+	    	green = (int) Math.floor(Math.random()*(255-100+1)+100);
+	    	blue = (int)  Math.floor(Math.random()*(255-100+1)+100);
 
-//		  //radius of ring
-		  //R = 300;
-//
-//		  //radius of disk
-		  //r =50;
-//
-//		  //distance from trace point in disk to center of disk
-		  //a = 20;
-		 // c = new Color((int)(Math.random() * 0x1000000));
-	   // 	R =  (float) Math.floor(Math.random()*(50-300+1)+50);
-	   // 	r =  (float) Math.floor(Math.random()*(1-100+1)+1);
-	   // 	a =  (float) Math.floor(Math.random()*(1-50+1)+1);
+	  
+	    	PVector thisOne = new PVector((int)Math.floor(R), (int)Math.floor(r), (int)Math.floor(a));
+	    	history.add(thisOne);
+			s = "R:" + R  + "r:" + r  + "a:" + a;	
 		  
-		  mouseClicked();
+		  
 	}
 
 public void mouseClicked() {
 	
 	
-    	path = new ArrayList<PVector>();
+	
+	if (mouseButton == LEFT){
+		itteration++;
+	}
+	else if (mouseButton == RIGHT) {
+		itteration--;
+		if (itteration<1) {
+			itteration=1;
+		}
+	}
+	
+	
+	//we want an previos spirographic
+	if (itteration < history.size()) {
+		path = new ArrayList<PVector>();
+		PVector thatOne = history.get(itteration-1);
+		R = thatOne.x;
+		r = thatOne.y;
+		a = thatOne.z;
+		s = "R:" + R  + "r:" + r  + "a:" + a;
+	}
+	
+	//we want an previos spirographic
+	if (itteration >=history.size()) {
+		path = new ArrayList<PVector>();
     	R =  (float) Math.floor(Math.random()*(200));
     	r =  (float) Math.floor(Math.random()*(200));
     	a =  (float) Math.floor(Math.random()*(200));
@@ -52,22 +79,56 @@ public void mouseClicked() {
     	red = (int) Math.floor(Math.random()*(255-100+1)+100);
     	green = (int) Math.floor(Math.random()*(255-100+1)+100);
     	blue = (int)  Math.floor(Math.random()*(255-100+1)+100);
-    	
-
-
+    	PVector newOne = new PVector((int)Math.floor(R), (int)Math.floor(r), (int)Math.floor(a));
+    	history.add(newOne);
 		s = "R:" + R  + "r:" + r  + "a:" + a;
+	}
+	
+	
+	
+	
+//	if (mouseButton == LEFT){
+//		
+//		if (itteration == history.size()){
+//		path = new ArrayList<PVector>();
+//    	R =  (float) Math.floor(Math.random()*(200));
+//    	r =  (float) Math.floor(Math.random()*(200));
+//    	a =  (float) Math.floor(Math.random()*(200));
+//    	
+//    	red = (int) Math.floor(Math.random()*(255-100+1)+100);
+//    	green = (int) Math.floor(Math.random()*(255-100+1)+100);
+//    	blue = (int)  Math.floor(Math.random()*(255-100+1)+100);
+//
+//  
+//    	PVector thisOne = new PVector((int)Math.floor(R), (int)Math.floor(r), (int)Math.floor(a));
+//    	history.add(thisOne);
+//		s = "R:" + R  + "r:" + r  + "a:" + a;
+//		itteration++;
+//		}
+//		else {
+//			path = new ArrayList<PVector>();
+//			PVector thatOne = history.get(itteration++);
+//			R = thatOne.x;
+//			r = thatOne.y;
+//			a = thatOne.z;
+//			s = "R:" + R  + "r:" + r  + "a:" + a;
+//		}
+//	}
+//	else if (mouseButton == RIGHT && itteration > 0) {
+//		path = new ArrayList<PVector>();
+//		PVector thatOne = history.get(--itteration);
+//		R = thatOne.x;
+//		r = thatOne.y;
+//		a = thatOne.z;
+//		s = "R:" + R  + "r:" + r  + "a:" + a;
+//		
+//	}
 		
 }
 	
 	
 	void update(){
 	    theta += resolution;
-	    //float x = (R+r) * cos(theta) + a * cos(((R-r)/r)*theta);
-	    //float y = (R+r) * sin(theta) - a * sin(((R-r)/r)*theta);
-	    
-	    //float x = (R+r) * cos(theta) - a * cos(((R-r)*theta)/r);
-	    //float y = (R+r) * sin(theta) - a * sin(((R-r)*theta)/r);
-	   
 	    
 	    float x = (R-r)*cos(theta) + a*cos(((R-r)/r)*theta);
 	    float y = (R-r)*sin(theta) - a*sin(((R-r)/r)*theta);
@@ -87,16 +148,11 @@ public void mouseClicked() {
 		  stroke(200,200,200,100);
 		  ellipse(300,300,R+R, R+R);
 		  
-
-
 		  //center of disk
 		  float innerCircleCenX = path.get(path.size()-1).x;
 		  float innerCircleCenY = path.get(path.size()-1).y;
-		  //float penX = innerCircleCenX + a * cos(theta);
-		  //float penY = innerCircleCenX + a * sin(theta);
 		  
-		  
-	      float rsum =  R - r;
+		  float rsum =  R - r;
 	      innerCircleCenX = 300 + rsum * cos(theta);
 	      innerCircleCenY = 300 + rsum * sin(theta);
 				  
@@ -115,6 +171,8 @@ public void mouseClicked() {
 		  for (PVector point : path){
 		    vertex(point.x, point.y);
 		    stroke(red,green,blue,255);
+		    
+		    //for looks
 		    //line(path.get(path.size()-1).x,path.get(path.size()-1).y, point.x, point.y);
 		  }
 		  endShape();
