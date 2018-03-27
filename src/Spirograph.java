9021b6f5441bddf317daa519fa19220ed89d887f
Shawn Sustrich <maxximillian@gmail.com>
2018-03-27 09:34:20 -0700

    Changed sized of window, added ability to press r just to change speed

--- src/Spirograph.java
+++ src/Spirograph.java
@@ -1,6 +1,9 @@
 import java.awt.Color;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
 import processing.core.*;
 
 public class Spirograph extends PApplet {
@@ -11,7 +14,7 @@ public class Spirograph extends PApplet {
 	
 	
 	private float theta = (float) 1.5708;
-	private float resolution = (float) .01;
+	private float resolution = (float) .1;
 	private float R = 0;
 	private float r = 0;
 	private float a = 0;
@@ -23,11 +26,26 @@ public class Spirograph extends PApplet {
 	private ArrayList<PVector> history =  new ArrayList<PVector>();
 	private Color c;
 	private int itteration = 1;
+	JTextField bigRingSize = new JTextField();
+	JTextField smallRingSize = new JTextField();
+	JTextField distance = new JTextField();
+	JTextField speedField = new JTextField();
 	
+	
+	Object[] message0 = {
+	    "Ring:", bigRingSize,
+	    "Disk", smallRingSize,
+	    "Distance", distance,
+	    "Speed", speedField
+	};
+
+	Object[] message1 = {
+		    "Speed", speedField
+		};
 	
 	public void settings() {
-		  size(600, 600);
-		 path = new ArrayList<PVector>();
+		  size(1000, 1000);
+    	 path = new ArrayList<PVector>();
 	    	R =  (float) Math.floor(Math.random()*(200));
 	    	r =  (float) Math.floor(Math.random()*(200));
 	    	a =  (float) Math.floor(Math.random()*(200));
@@ -44,10 +62,42 @@ public class Spirograph extends PApplet {
 		  
 	}
 
+	public void keyPressed() {
+		if (keyCode == 32) {
+				save("C:/users/ssustrich/desktop/" +R+"-"+r+"-"+a+".png");
+	}
+				
+		if(keyCode == ENTER)
+	      {
+			speedField.setText(String.valueOf(resolution));
+			int option = JOptionPane.showConfirmDialog(null, message0, "Sizes", JOptionPane.OK_CANCEL_OPTION);
+			if (option == JOptionPane.OK_OPTION) {
+				path = new ArrayList<PVector>();
+				int big = Integer.valueOf((bigRingSize.getText()));
+	    	    int small = Integer.valueOf((smallRingSize.getText()));
+	    	    int dist = Integer.valueOf((distance.getText()));
+	    	    resolution = Float.valueOf(speedField.getText());
+		      	PVector newOne = new PVector(big, small, dist);
+		    	history.add(newOne);
+		    	setUpForNextShape(newOne);
+				itteration = history.size();
+			} 
+	      }
+		
+		if(keyCode == 82 ||keyCode== 114)
+	      {
+			speedField.setText(String.valueOf(resolution));
+			int option = JOptionPane.showConfirmDialog(null, message1, "Resolution", JOptionPane.OK_CANCEL_OPTION);
+			if (option == JOptionPane.OK_OPTION) {
+				path = new ArrayList<PVector>();
+	    	    resolution = Float.valueOf(speedField.getText());
+			} 
+	      }
+		}
+	
+	
+	
 public void mouseClicked() {
-	
-	
-	
 	if (mouseButton == LEFT){
 		itteration++;
 	}
@@ -57,83 +107,14 @@ public void mouseClicked() {
 			itteration=1;
 		}
 	}
-	
-	
-	//we want an previos spirographic
-	if (itteration < history.size()) {
-		path = new ArrayList<PVector>();
-		PVector thatOne = history.get(itteration-1);
-		R = thatOne.x;
-		r = thatOne.y;
-		a = thatOne.z;
-		s = "R:" + R  + "r:" + r  + "a:" + a;
-	}
-	
-	//we want an previos spirographic
-	if (itteration >=history.size()) {
-		path = new ArrayList<PVector>();
-    	R =  (float) Math.floor(Math.random()*(200));
-    	r =  (float) Math.floor(Math.random()*(200));
-    	a =  (float) Math.floor(Math.random()*(200));
-    	
-    	red = (int) Math.floor(Math.random()*(255-100+1)+100);
-    	green = (int) Math.floor(Math.random()*(255-100+1)+100);
-    	blue = (int)  Math.floor(Math.random()*(255-100+1)+100);
-    	PVector newOne = new PVector((int)Math.floor(R), (int)Math.floor(r), (int)Math.floor(a));
-    	history.add(newOne);
-		s = "R:" + R  + "r:" + r  + "a:" + a;
-	}
-	
-	
-	
-	
-//	if (mouseButton == LEFT){
-//		
-//		if (itteration == history.size()){
-//		path = new ArrayList<PVector>();
-//    	R =  (float) Math.floor(Math.random()*(200));
-//    	r =  (float) Math.floor(Math.random()*(200));
-//    	a =  (float) Math.floor(Math.random()*(200));
-//    	
-//    	red = (int) Math.floor(Math.random()*(255-100+1)+100);
-//    	green = (int) Math.floor(Math.random()*(255-100+1)+100);
-//    	blue = (int)  Math.floor(Math.random()*(255-100+1)+100);
-//
-//  
-//    	PVector thisOne = new PVector((int)Math.floor(R), (int)Math.floor(r), (int)Math.floor(a));
-//    	history.add(thisOne);
-//		s = "R:" + R  + "r:" + r  + "a:" + a;
-//		itteration++;
-//		}
-//		else {
-//			path = new ArrayList<PVector>();
-//			PVector thatOne = history.get(itteration++);
-//			R = thatOne.x;
-//			r = thatOne.y;
-//			a = thatOne.z;
-//			s = "R:" + R  + "r:" + r  + "a:" + a;
-//		}
-//	}
-//	else if (mouseButton == RIGHT && itteration > 0) {
-//		path = new ArrayList<PVector>();
-//		PVector thatOne = history.get(--itteration);
-//		R = thatOne.x;
-//		r = thatOne.y;
-//		a = thatOne.z;
-//		s = "R:" + R  + "r:" + r  + "a:" + a;
-//		
-//	}
-		
+	setUpForNextShape();
 }
-	
 	
 	void update(){
 	    theta += resolution;
-	    
 	    float x = (R-r)*cos(theta) + a*cos(((R-r)/r)*theta);
 	    float y = (R-r)*sin(theta) - a*sin(((R-r)/r)*theta);
-	    
-	    PVector pv = new PVector(x+300,y+300);
+	    PVector pv = new PVector(x+500,y+500);
 		path.add(pv);
 	}	
 	
@@ -141,27 +122,69 @@ public void mouseClicked() {
 
 	}
 
+	
+	
+	public void setUpForNextShape(PVector points) {
+		path = new ArrayList<PVector>();
+		R =  points.x;
+		r = points.y;
+		a = points.z;
+    	red = (int) Math.floor(Math.random()*(255-100+1)+100);
+    	green = (int) Math.floor(Math.random()*(255-100+1)+100);
+    	blue = (int)  Math.floor(Math.random()*(255-100+1)+100);
+		s = "R:" + R  + "r:" + r  + "a:" + a;
+	}
+	
+	
+	
+	public void setUpForNextShape() {
+		//we want an previos spirographic
+		if (itteration <= history.size()) {
+			PVector thatOne = history.get(itteration-1);
+			setUpForNextShape(thatOne);
+		}
+		//we want a new spirographic
+		else if (itteration > history.size()) {
+	    	R =  (float) Math.floor(Math.random()*(200));
+	    	r =  (float) Math.floor(Math.random()*(200));
+	    	a =  (float) Math.floor(Math.random()*(200));
+			PVector newOne = new PVector((int)Math.floor(R), (int)Math.floor(r), (int)Math.floor(a));
+	    	history.add(newOne);
+	    	setUpForNextShape(newOne);
+		}
+	}
+	
+	
+	
 	public void draw() {
+        if (millis() < 1000) {
+            ((java.awt.Canvas) surface.getNative()).requestFocus();
+        }
+		   
 		  background(55);
 		  update();
 		  text(s, 10, 590);
+		  text(resolution, 10, 10);
 		  stroke(200,200,200,100);
-		  ellipse(300,300,R+R, R+R);
+		  
+		  
+		  //center of ring
+		  ellipse(500,500,R+R, R+R);
 		  
 		  //center of disk
 		  float innerCircleCenX = path.get(path.size()-1).x;
 		  float innerCircleCenY = path.get(path.size()-1).y;
 		  
 		  float rsum =  R - r;
-	      innerCircleCenX = 300 + rsum * cos(theta);
-	      innerCircleCenY = 300 + rsum * sin(theta);
+	      innerCircleCenX = 500 + rsum * cos(theta);
+	      innerCircleCenY = 500 + rsum * sin(theta);
 				  
 	      
 		  ellipse(innerCircleCenX,innerCircleCenY,r*2, r*2);
 		  ellipse(path.get(path.size()-1).x,path.get(path.size()-1).y,5, 5);
 		  
 		  //line from origin to center of disk
-		  line(300,300, innerCircleCenX, innerCircleCenY);
+		  line(500,500, innerCircleCenX, innerCircleCenY);
 		  //line from center of disk to pen circle
 		  line(path.get(path.size()-1).x,path.get(path.size()-1).y, innerCircleCenX, innerCircleCenY);
 		  noFill();
@@ -176,5 +199,7 @@ public void mouseClicked() {
 		    //line(path.get(path.size()-1).x,path.get(path.size()-1).y, point.x, point.y);
 		  }
 		  endShape();
+
+		  
 	}
 }
